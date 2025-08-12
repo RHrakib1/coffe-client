366f6e2c21a275a2c9328dc47b8a2bd148917e3c
RHrakib1 <rakibhasanmd457@gmail.com>
2025-08-12 23:36:02 +0600

    add to form

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 
-import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <div className="flex w-full justify-center">
-        <kbd className="kbd">▲</kbd>
-      </div>
-      <div className="flex w-full justify-center gap-12">
-        <kbd className="kbd">◀︎</kbd>
-        <kbd className="kbd">▶︎</kbd>
-      </div>
-      <div className="flex w-full justify-center">
-        <kbd className="kbd">▼</kbd>
-      </div>
+
       <h2 className='text-7xl text-blue-400 font-extrabold '>hi</h2>
     </>
   )
--- src/Component/Addcoffe.jsx
+++ src/Component/Addcoffe.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 
 export default function Addcoffe() {
+    const hendlesubmit = e => {
+        e.preventDefault()
+        const datastore = e.target
+        const name = datastore.name.value
+        const able = datastore.able.value
+        const supply = datastore.supply.value
+        const catagory = datastore.catagory.value
+        const objdata = { name, able, supply, catagory }
+        console.log(objdata)
+    }
     return (
-        <div>
-            <h1>Addcoffe</h1>
+        <div className='bg-gray-300 p-24 max-w-5xl rounded-4xl m-auto mt-10'>
+            <h1 className='text-3xl font-bold text-center mb-5'>Add Coffee</h1>
+            <form onSubmit={hendlesubmit} >
+                {/* {row no 1} */}
+                <div className='flex justify-between'>
+                    <div className="flex gap-2 mb-4 items-center md:w-1/2" >
+                        <h2 className='mr-2'>Name:</h2>
+                        <div>
+                            <label className="">
+                                <input type="text" name='name' placeholder="Input your coffe name" required className='w-full border p-2' />
+                            </label>
+                        </div>
+                    </div>
+                    <div className=" flex gap-2 items-center md:w-1/2">
+                        <h2 className='mr-2'>Available:</h2>
+                        <div>
+                            <label className="">
+                                <input type="text" name='able' placeholder="add your quantity" required className='w-full border p-2' />
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                {/* {row no 2} */}
+                <div className='flex justify-between '>
+                    <div className="flex gap-2 mb-4 items-center md:w-1/2" >
+                        <h2 className='mr-2'>Supply:</h2>
+                        <div>
+                            <input type="text" name='supply' placeholder="Input your coffe supplyer name" required className='w-full border p-2' />
+                        </div>
+                    </div>
+                    <div className=" flex gap-2 items-center md:w-1/2">
+                        <h2 className='mr-2'>Catagory:</h2>
+                        <div>
+                            <input type="text" name='catagory' placeholder="add your catagory" required className='w-full border p-2' />
+                        </div>
+                    </div>
+                </div>
+                <input type="submit" value='ADD COFFEE' className='btn btn-block' />
+            </form>
         </div>
     )
 }
