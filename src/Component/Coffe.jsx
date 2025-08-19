389eceb5c2c35fac13657190499311f56e206238
RHrakib1 <rakibhasanmd457@gmail.com>
2025-08-19 12:20:00 +0600

    add icon

--- src/Component/Coffe.jsx
+++ src/Component/Coffe.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import Swal from 'sweetalert2'
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { GrView } from "react-icons/gr";
+import { FiEdit } from "react-icons/fi";
+
 
 export default function Coffe({ Coffe }) {
-    const { id, name, able, supply, catagory, photourl } = Coffe
+    const { _id, name, able, supply, catagory, photourl } = Coffe
+
+    const heandelDelete = id => {
+        console.log('the deletet id is a :', id)
+        fetch(`http://localhost:5000/postcoffedata/${id}`, {
+            method: "DELETE"
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.deletedCount > 0) {
+                    Swal.fire({
+                        title: 'Delete!',
+                        text: 'You have successsully delete',
+                        icon: 'warning',
+                        confirmButtonText: 'Cool'
+                    })
+                }
+            })
+    }
     return (
         <div>
             <div className="flex gap-3 bg-gray-700 shadow-xl">
@@ -13,9 +36,9 @@ export default function Coffe({ Coffe }) {
                 </div>
                 <div className="card-body">
                     <div className="card-actions flex flex-col justify-end">
-                        <button className="btn btn-primary">Edit</button>
-                        <button className="btn btn-primary">View</button>
-                        <button className="btn btn-primary">Delete</button>
+                        <button className="btn btn-primary"><FiEdit className='text-xl'></FiEdit></button>
+                        <button className="btn btn-primary"><GrView className='text-xl'></GrView></button>
+                        <button onClick={() => heandelDelete(_id)} className="btn btn-primary"><RiDeleteBin6Line className='text-xl'></RiDeleteBin6Line></button>
                     </div>
                 </div>
             </div>
